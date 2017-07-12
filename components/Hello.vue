b46d14d740fa5e2470c38798625d0c9caf4fb156
王林涛 <hzwanglintao@corp.netease.com>
2017-07-12 12:06:42 +0800

    添加readme

--- components/Hello.vue
+++ components/Hello.vue
@@ -14,7 +14,7 @@
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-1111111111</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-33333333</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
