a34704fe18ff5ca11268347645326fafb3811941
王林涛 <hzwanglintao@corp.netease.com>
2017-07-12 11:30:34 +0800

    sf-webpack-vue 问题  https://segmentfault.com/q/1010000010140591?_ea=2180593

---
+++ components/xixi.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+        AAAAA
+  </div>
+</template>
+<style>
+  .time-clock input {
+    width: 46px;
+    height: 24px;
+    text-align: center;
+    border: 1px solid #d9d9d9;
+    border-radius: 0;
+    color: black;
+  }
+
+  .time-clock .colon {
+    font-weight: 700;
+    font-size: 12px;
+  }
+</style>
+<script>
+
+  export default {
+    props: {
+        row: Object
+    }
+  }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack');
+var path = require('path');
+console.log(path.resolve(__dirname, 'index.js'))
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js',
+          '@': __dirname
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(png)$/,
+                loader: 'url-loader?limit=8192'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ],
+    devServer: {
+        port: 8050,
+        host: 'localhost',
+        inline: true,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname),
+        publicPath: '/',
+        proxy: {
+        }
+    }
+};
+
